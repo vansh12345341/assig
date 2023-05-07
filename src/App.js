ec7697acb0e3ecf44385dbf1d3439451b0680d92
Vansh <vansh432.sharma@gmail.com>
2023-05-07 18:37:30 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,12 +1,39 @@
-import { useState } from 'react'
-import './App.css';
+import React, { useState } from "react";
 
 function App() {
+  const [items, setItems] = useState([
+    "First",
+    "Second",
+    "Third"
+  ]);
+  const [newItemText, setNewItemText] = useState("");
 
-  const items 
+  const handle= (event) => {
+    event.preventDefault();
+    if (newItemText.trim() !== "") {
+      setItems([...items, newItemText]);
+      setNewItemText("");
+    }
+  };
+
   return (
-    <div >
-     
+    <div>
+    
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <form onSubmit={handle}>
+        <label htmlFor="new-item">Add new item:</label>
+        <input
+          type="text"
+          id="new-item"
+          value={newItemText}
+          onChange={(event) => setNewItemText(event.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
     </div>
   );
 }
